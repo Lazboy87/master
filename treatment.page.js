95c371d13c7155d50f54a4a7227f3cf2121bbe77
Blixits <andreassaxvik@hotmail.com>
2019-11-08 00:29:50 +0100

    added basic structure
    Added the basic structure for mobile platform.

--- treatment.page.js
+++ treatment.page.js
@@ -1,14 +1,35 @@
 export default{
     template:` 
-    <div>
-    <h1>Velg Behandling</h1>
-    <router-link to="/dateaku">Akupunktur</router-link>
-    <router-link to="/dateoso">Osteopati</router-link>
-    <router-link to="/datefyso">Fysiologisk Testlab</router-link>
-    <router-link to="/datekost">Kostholdsveiledning</router-link>
-    
-    </div>`
-   ,
+    <div class="mainContainer">
+        <h1 class="hTag">Velg Behandling</h1>
+
+
+        <div class="routerContainer">
+        <router-link to="/dateaku" class="elLink">Akupunktur</router-link>
+        </div>
+        
+        <div class="routerContainer">
+        <router-link to="/dateoso" class="elLink">Osteopati</router-link>
+        </div>
+        
+        <div class="routerContainer">
+        <router-link to="/datefyso" class="elLink">Fysiologisk Testlab</router-link>
+        </div>
+        
+        <div class="routerContainer">
+        <router-link to="/datekost" class="elLink">Kostholdsveiledning</router-link>
+            <div>
+                <img>
+            </div>
+        </div>
+    </div>
+    `,
     
     
     };
+    // <div class="test">
+    //<router-link to="/dateaku">Akupunktur</router-link>
+    //</div>
+    //<router-link to="/dateoso">Osteopati</router-link>
+    //<router-link to="/datefyso">Fysiologisk Testlab</router-link>
+    //<router-link to="/datekost">Kostholdsveiledning</router-link>
