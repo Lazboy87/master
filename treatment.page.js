1d5855d6eaf78d13890fc85735dae19b66a6c103
Blixits <andreassaxvik@hotmail.com>
2019-11-09 21:52:45 +0100

    Added possibility to show and hide info div
    added the possibility to show and hide the info divs.

--- treatment.page.js
+++ treatment.page.js
@@ -1,35 +1,68 @@
 export default{
     template:` 
     <div class="mainContainer">
-        <h1 class="hTag">Velg Behandling</h1>
+
+        <img class="mainLogo" src="Images/logo.png">
+        <h1 class="hTag">{{title}}</h1>
 
 
         <div class="routerContainer">
         <router-link to="/dateaku" class="elLink">Akupunktur</router-link>
+        <img v-on:click="seen = !seen" class="Images" id="img1" src="Images/fillerPicForQuestionMark119-364x364.jpg"/>
         </div>
+
+            <div v-if="seen" class="testPopup">
+            <p v-if="seen">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
         
         <div class="routerContainer">
         <router-link to="/dateoso" class="elLink">Osteopati</router-link>
+        <img v-on:click="seen2 = !seen2" class="Images" id="img2" src="Images/fillerPicForQuestionMark119-364x364.jpg"/>
         </div>
+
+            <div v-if="seen2" class="testPopup">
+            <p v-if="seen2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
         
         <div class="routerContainer">
         <router-link to="/datefyso" class="elLink">Fysiologisk Testlab</router-link>
+        <img v-on:click="seen3 = !seen3" class="Images" id="img3" src="Images/fillerPicForQuestionMark119-364x364.jpg"/>
         </div>
+
+            <div v-if="seen3" class="testPopup">
+            <p v-if="seen3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
         
         <div class="routerContainer">
         <router-link to="/datekost" class="elLink">Kostholdsveiledning</router-link>
-            <div>
-                <img>
-            </div>
+        <img v-on:click="seen4 = !seen4" class="Images" src="Images/fillerPicForQuestionMark119-364x364.jpg"/>
         </div>
+            <div v-if="seen4" class="testPopup">
+            <p v-if="seen4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
     </div>
     `,
-    
-    
+    data(){
+        return{
+            title: "Velg Behandling",
+            seen: false, 
+            seen2: false,
+            seen3: false,
+            seen4: false
+
+        }
+    },
+    methods:{
+        info(){
+            alert("Hello World");
+        } 
+    }
     };
+
     // <div class="test">
     //<router-link to="/dateaku">Akupunktur</router-link>
     //</div>
     //<router-link to="/dateoso">Osteopati</router-link>
     //<router-link to="/datefyso">Fysiologisk Testlab</router-link>
     //<router-link to="/datekost">Kostholdsveiledning</router-link>
+    //<img class="Images" src="Images/fillerPicForQuestionMark119-364x364.jpg"/>
